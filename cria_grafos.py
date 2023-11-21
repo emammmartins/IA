ec4d3f2dff8d7aefa2344e8cfadb18bc31599fc6
Marta <a100593@alunos.uminho.pt>
2023-11-21 21:25:32 +0000

    Heurísticas

--- cria_grafos.py
+++ cria_grafos.py
@@ -77,12 +77,18 @@ def cria_grafo():
         G.add_edge("Nogueira", "Lomar", weight=8)
         G.add_edge("Lomar", "SãoLázaro", weight=6)
 
-        G.nodes['Armazem']['heuristica'] = {'SãoVicente': 11.6, 'Gualtar': 6, "Este":4,"SãoVitor":10.4,"SãoJoãoDoSouto":12,"Tenões":9.6,"Espinho":11.8,"SãoLázaro":14,"Lamaçães":12.8,"Nogueiró":11.8,"Fraião":15}
-        G.nodes['SãoVicente']['heuristica']={"Gualtar":5,"SãoVitor":4,"Este":9,"SãoJoãoDoSouto":3,"Tenões":7.4,"SãoVicente":7.2,"Nogueiró":10.8,"Lamaçães":8.8,"Fraião":10,"SãoLázara":6.2}
-        G.nodes['Gualtar']['heuristica']={"SãoVitor":4.8,"SãoJoãoDoSouto":6.4,"Este":4,"Tenões":4,"Espinho":9,"Nogueiró":7.4,"Lamaçães":7,"Faião":9,"SãoLázaro":7.4}
-        G.nodes['SãoVitor']['heuristica']={"SãoJoãoDoSouto":3.8,"Tenões":4.4,"Espinho":4.4,"Nogueiró":7,"Lamaçães":5,"Fraião":6,"SãoLázaro":3,"Este":7}
-        G.nodes['SãoJoãoDoSouto']['heuristica']={"SãoLázaro":4,"Fraião":8,"Lamaçães":7.2,"Nogueiró":10,"Espinho":13.2,"Tenões":7,"Este":9.2}
-        G.nodes['SãoLázaro']['heuristica']={"Tenões":5.8,"Este":8.6,"Espinho":11,"Nogueiró":7.2,"Lamaçães":4.4,"Fraião":3.8}
-
+        G.nodes['Armazem']['heuristica'] = {'Armazem':0, 'SãoVicente':9.1, 'Gualtar':5.8, "Este":6.8,"SãoVitor":10,"SãoJoãoDoSouto":11.5,"Tenões":9,"Espinho":11.4,"SãoLázaro":11,"Lamaçães":12.4,"Nogueiró":11.8,"Fraião":14.9}
+        G.nodes['SãoVicente']['heuristica']={'Armazem':9.1, 'SãoVicente':0, "Gualtar":4.7,"Este":8.3,"SãoVitor":3.6,"SãoJoãoDoSouto":2.8,"Tenões":7.2,"Espinho":13,"SãoLázaro":6,"Lamaçães":8.3,"Nogueiró":10.2,"Fraião":9.6}
+        G.nodes['Gualtar']['heuristica']= {'Armazem':5.8, 'SãoVicente':4.7, "Gualtar":0,"Este":3.6,"SãoVitor":4,"SãoJoãoDoSouto":5.9,"Tenões":3.8,"Espinho":8.5,"SãoLázaro":7.1,"Lamaçães":6.6,"Nogueiró":6.7,"Fraião":8.8}
+        G.nodes['Este']['heuristica']= {'Armazem':6.8, 'SãoVicente':8.3, "Gualtar":3.6,"Este":0,"SãoVitor":6.6,"SãoJoãoDoSouto":9,"Tenões":2.8,"Espinho":4.8,"SãoLázaro":8.4,"Lamaçães":6,"Nogueiró":4.4,"Fraião":8}
+        G.nodes['SãoVitor']['heuristica']= {'Armazem':10, 'SãoVicente':3.6, "Gualtar":4,"Este":6.6,"SãoVitor":0,"SãoJoãoDoSouto":2.4,"Tenões":4,"Espinho":10.1,"SãoLázaro":2.8,"Lamaçães":4.6,"Nogueiró":6.8,"Fraião":6}
+        G.nodes['SãoJoãoDoSouto']['heuristica']= {'Armazem':11.5, 'SãoVicente':2.8, "Gualtar":5.9,"Este":9,"SãoVitor":2.4,"SãoJoãoDoSouto":0,"Tenões":6.6,"Espinho":12.7,"SãoLázaro":3.7,"Lamaçães":6.9,"Nogueiró":9.4,"Fraião":7.5}
+        G.nodes['Tenões']['heuristica']= {'Armazem':9, 'SãoVicente':7.2, "Gualtar":3.8,"Este":2.8,"SãoVitor":4,"SãoJoãoDoSouto":6.6,"Tenões":0,"Espinho":5.8,"SãoLázaro":5.2,"Lamaçães":2.8,"Nogueiró":2.7,"Fraião":5}
+        G.nodes['Espinho']['heuristica']= {'Armazem':11.4, 'SãoVicente':13, "Gualtar":8.5,"Este":4.8,"SãoVitor":10.1,"SãoJoãoDoSouto":12.7,"Tenões":5.8,"Espinho":0,"SãoLázaro":10.7,"Lamaçães":6.8,"Nogueiró":3.8,"Fraião":8.5}
+        G.nodes['SãoLázaro']['heuristica']= {'Armazem':11, 'SãoVicente':6, "Gualtar":7.1,"Este":8.4,"SãoVitor":2.8,"SãoJoãoDoSouto":3.7,"Tenões":5.2,"Espinho":10.7,"SãoLázaro":0,"Lamaçães":3.8,"Nogueiró":7,"Fraião":3.6}
+        G.nodes['Lamaçães']['heuristica']= {'Armazem':12.4, 'SãoVicente':8.3, "Gualtar":6.6,"Este":6,"SãoVitor":4.6,"SãoJoãoDoSouto":6.9,"Tenões":2.8,"Espinho":6.8,"SãoLázaro":3.8,"Lamaçães":0,"Nogueiró":2.8,"Fraião":1.7}
+        G.nodes['Nogueiró']['heuristica']= {'Armazem':11.8, 'SãoVicente':10.2, "Gualtar":6.7,"Este":4.4,"SãoVitor":6.8,"SãoJoãoDoSouto":9.4,"Tenões":2.7,"Espinho":3.8,"SãoLázaro":7,"Lamaçães":2.8,"Nogueiró":0,"Fraião":4.6}
+        G.nodes['Fraião']['heuristica']= {'Armazem':14.9, 'SãoVicente':9.6, "Gualtar":8.8,"Este":8,"SãoVitor":6,"SãoJoãoDoSouto":7.5,"Tenões":5,"Espinho":8.5,"SãoLázaro":3.6,"Lamaçães":1.7,"Nogueiró":4.6,"Fraião":0}
+        
 
         return G
